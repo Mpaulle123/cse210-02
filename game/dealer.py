ef5cfd320edf015d75f112d881a2a2c8cfc1968c
Mpaulle22 <mah22003@byui.edu>
2022-10-08 16:34:30 +0200

    added comments

--- game/dealer.py
+++ game/dealer.py
@@ -1,23 +1,28 @@
 import random
 
+
 class Dealer:
-    """this class will control the display of the cards"""
-
-    def  __init__(self) :
-        """ self is the parameter and self.number_card is the instance"""
-        
-        self.number_card = 0
-        
-        
-        
-    def random_card(self):
-        """here i will generate the random number and display it for card 1 and 2"""
-
-        self.number_card = random.randint(1,13)
-        return self.number_card
-        
+    """
+    The class "Dealer" will control the card numbers (from 1 to 13) and display randomly the first and next card.
     
 
+    Attributes
+    number_card (int): set cards from 1 to 13
 
+    """
 
-        
+    def __init__(self):
+        """
+        Constructs a new instance of Dealer.
+        Args:
+        self (Dealer): An instance of Dealer.
+        """
+        self.number_card = 0
+
+    def random_card(self):
+        """
+        It returns a random number between 1 and 13
+        :return: The number of the card.
+        """
+        self.number_card = random.randint(1, 13)
+        return self.number_card
--- game/director.py
+++ game/director.py
@@ -2,8 +2,11 @@
 from game.dealer import Dealer
 
 class Director:
-    """here i will try to define the code to link both classes"""
-
+    """
+    The class "Director" will control the game flow, initialize the points of the player with 300 points,
+    evaluate the responses of the player and compute the score.
+    
+    """
     def __init__(self):
         
         self.guess = None
@@ -15,9 +18,23 @@ class Director:
         
 
     def start_game(self):
-        """it start and display the text for the beginning of the game"""
-        
+        """
+        The function starts the game by displaying a card number between 1 to 13.
+
+        The function then asks the user to guess whether the next card will be higher or lower.
+
+        The function then evaluates the user's guess and updates the score.
+
+        The function then asks the user if they want to play again.
+
+        If the user's score is greater than 0, the user can play again.
+
+        If the user's score is less than or equal to 0, the game is over.
+        """
+
         while self.play_again == "y":
+            #the while loop will restart the game at this point 
+            # dealer is new instance of Dealer
             dealer = Dealer()
             self.current_card = dealer.random_card()
             print("\nWelcom to Hilo Game.")
@@ -28,6 +45,7 @@ class Director:
             self.validation = False
 
             while self.validation == False:
+    
                 self.next_card = dealer.random_card()
                 self.guess = input("Higher or Lower? (h/l) ")
                 self.evaluate_guess()
@@ -42,6 +60,12 @@ class Director:
                 break
 
     def evaluate_guess(self):
+        """
+        If the guess is either "h" or "l", then compute the score and set validation to True. Otherwise,
+        print "Wrong answer... Try again." and set validation to False.
+        :return: The validation variable is being returned.
+        """
+        
         if (self.guess == "h" or self.guess == "l"):
             self.compute_score()
             self.validation = True
@@ -52,7 +76,11 @@ class Director:
             return self.validation
 
     def compute_score(self):
-        
+        """
+        If the guess is correct, the score is increased by 100. If the guess is incorrect, the score is
+        decreased by 70
+        :return: The score is being returned.
+        """
 
         if self.guess == "h" and self.next_card > self.current_card:
             self.score = self.score + 100
